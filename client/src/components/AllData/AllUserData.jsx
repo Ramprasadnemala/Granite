c9e5fe9533ab2be83c4175ecf39a2f8c2364d288
Ramprasadnemala <Ramprasadnemala12@gmail.com>
2024-06-05 00:37:44 +0530

    form changes

--- client/src/components/AllData/AllUserData.jsx
+++ client/src/components/AllData/AllUserData.jsx
@@ -40,14 +40,17 @@ const AllUserData = () => {
 
     if (!formData.date) errors.date = 'Date is required';
     if (!formData.DC_No) errors.DC_No = 'DC No is required';
-    if (!formData.vehicleNo) errors.vehicleNo = 'Vehicle No is required';
-    if (!formData.materialType) errors.materialType = 'Material Type is required';
-    if (!formData.transportName) errors.transportName = 'Transport Name is required';
-    if (!formData.sourcePlace) errors.sourcePlace = 'Source Place is required';
+    if (!formData.vehicleNo) errors.vehicleNo = 'VehicleNo is required';
+    if (!formData.materialType) errors.materialType = 'MaterialType is required';
+    if (!formData.transportName) errors.transportName = 'TransportName is required';
+    if (!formData.sourcePlace) errors.sourcePlace = 'SourcePlace is required';
     if (!formData.gross) errors.gross = 'Gross is required';
     if (!formData.tare) errors.tare = 'Tare is required';
-    if (!formData.loadingTime) errors.loadingTime = 'Loading Time is required';
-    if (!formData.unloadingTime) errors.unloadingTime = 'Unloading Time is required';
+    if (!formData.net) errors.net = 'Net is required';
+    if (!formData.royalty) errors.royalty = 'Royalty is required';
+    if (!formData.loadingTime) errors.loadingTime = 'LoadingTime is required';
+    if (!formData.unloadingTime) errors.unloadingTime = 'UnloadingTime is required';
+    if (!formData.remarks) errors.remarks = 'Remarks is required';
 
     setFormErrors(errors);
     return Object.keys(errors).length === 0;
@@ -82,7 +85,7 @@ const AllUserData = () => {
           sourcePlace: '',
           gross: '',
           tare: '',
-          net: '',
+          net:'',
           loadingTime: '',
           unloadingTime: '',
           royalty: '',
@@ -105,17 +108,17 @@ const AllUserData = () => {
         <div className="full-width">
           <label>Date:</label>
           <input type="date" name="date" value={formData.date} onChange={handleChange} />
-          {formErrors.date && <span className="error">{formErrors.date}</span>}
+          {formErrors.date && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.date}</span>}
         </div>
         <div className="full-width">
           <label>DC No:</label>
           <input type="text" name="DC_No" placeholder="please enter DC No" value={formData.DC_No} onChange={handleChange} />
-          {formErrors.DC_No && <span className="error">{formErrors.DC_No}</span>}
+          {formErrors.DC_No && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.DC_No}</span>}
         </div>
         <div className="full-width">
           <label>Vehicle No:</label>
           <input type="text" name="vehicleNo" placeholder="please enter vehicleNo" value={formData.vehicleNo} onChange={handleChange} />
-          {formErrors.vehicleNo && <span className="error">{formErrors.vehicleNo}</span>}
+          {formErrors.vehicleNo && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.vehicleNo}</span>}
         </div>
         <div className="full-width3">
           <label>Material Type:</label>
@@ -126,27 +129,27 @@ const AllUserData = () => {
             <option value="3ton to 6ton">3ton to 6ton</option>
             <option value="6ton to 10ton">6ton to 10ton</option>
           </select>
-          {formErrors.materialType && <span className="error">{formErrors.materialType}</span>}
+          {formErrors.materialType && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.materialType}</span>}
         </div>
         <div className="full-width">
           <label>Transport:</label>
           <input type="text" name="transportName" placeholder="please enter transport" value={formData.transportName} onChange={handleChange} />
-          {formErrors.transportName && <span className="error">{formErrors.transportName}</span>}
+          {formErrors.transportName && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.transportName}</span>}
         </div>
         <div className="full-width">
           <label>Source of Place:</label>
           <input type="text" name="sourcePlace" placeholder="please enter source" value={formData.sourcePlace} onChange={handleChange} />
-          {formErrors.sourcePlace && <span className="error">{formErrors.sourcePlace}</span>}
+          {formErrors.sourcePlace && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.sourcePlace}</span>}
         </div>
         <div className="full-width">
           <label>Gross:</label>
           <input type="number" name="gross" placeholder="please enter gross" value={formData.gross} onChange={handleChange} />
-          {formErrors.gross && <span className="error">{formErrors.gross}</span>}
+          {formErrors.gross && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.gross}</span>}
         </div>
         <div className="full-width">
           <label>Tare:</label>
           <input type="number" name="tare" placeholder="please enter tare" value={formData.tare} onChange={handleChange} />
-          {formErrors.tare && <span className="error">{formErrors.tare}</span>}
+          {formErrors.tare && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.tare}</span>}
         </div>
         <div className="full-width">
           <label>Net:</label>
@@ -155,24 +158,26 @@ const AllUserData = () => {
         <div className="full-width">
           <label>Royalty:</label>
           <input type="number" name="royalty" placeholder="please enter royalty" value={formData.royalty} onChange={handleChange} />
+          {formErrors.royalty && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.royalty}</span>}
         </div>
         <div className="full-width">
           <label>Loading Time:</label>
           <input type="time" name="loadingTime" value={formData.loadingTime} onChange={handleChange} />
-          {formErrors.loadingTime && <span className="error">{formErrors.loadingTime}</span>}
+          {formErrors.loadingTime && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.loadingTime}</span>}
         </div>
         <div className="full-width">
           <label>Unloading Time:</label>
           <input type="time" name="unloadingTime" value={formData.unloadingTime} onChange={handleChange} />
-          {formErrors.unloadingTime && <span className="error">{formErrors.unloadingTime}</span>}
+          {formErrors.unloadingTime && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.unloadingTime}</span>}
         </div>
         <div className="full-width2">
           <label>Remarks:</label>
           <textarea name="remarks" placeholder="please enter remarks" value={formData.remarks} onChange={handleChange}></textarea>
+          {formErrors.remarks && <span className="error" style={{fontSize:"12px",color:"red"}}>*{formErrors.remarks}</span>}
         </div>
         <button type="submit">Submit</button>
       </form>
-      {submissionStatus && <p>{submissionStatus}</p>}
+      {submissionStatus && <p style={{fontSize:"14px",color:"rgb(116, 114, 114)"}}>{submissionStatus}</p>}
     </div>
   );
 };
--- client/src/components/headerSelection/PopupForm/PopupForm.jsx
+++ client/src/components/headerSelection/PopupForm/PopupForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './PopupForm.css'; // Import CSS file for styling
-import { Link } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import icons
+import { Link } from 'react-router-dom';
+
 
 const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,8 @@ const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
     password: ''
   });
   const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [generalError, setGeneralError] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,25 +19,63 @@ const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
       ...prevData,
       [name]: value
     }));
+    setGeneralError(''); // Clear the general error when user starts typing
+  };
+
+  const handleFocus = (e) => {
+    const { name } = e.target;
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [name]: ''
+    }));
+    setGeneralError(''); // Clear the general error when field is focused
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.name.trim()) {
+      newErrors.name = 'Name is required';
+    }
+    if (!formData.password) {
+      newErrors.password = 'Password is required';
+    }
+    return newErrors;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you can handle form submission, e.g., send data to backend
-    console.log(formData);
-    // Close the popup
-    onClose();
-  };
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      const validCredentials = [
+        { name: "murali", password: "murali@@1002" },
+        { name: "user", password: "user@@1002" }
+      ];
 
-  const formClose = (e) => {
-    onClose();
-    setLoggedIn(true);
+      const isValidUser = validCredentials.some(
+        cred => cred.name.toLowerCase() === formData.name.toLowerCase() && cred.password === formData.password
+      );
+
+      if (isValidUser) {
+        // Close the popup
+        onClose();
+        setLoggedIn(true);
+      } else {
+        setGeneralError('Invalid credentials. Please enter the correct');
+      }
+    }
   };
 
   const togglePasswordVisibility = () => {
     setShowPassword(prevShowPassword => !prevShowPassword);
   };
 
+
+  const formClose = (e) => {
+    onClose();
+    setLoggedIn(true);
+  };
   return (
     <div className="popup2002">
       <div className="popup-inner2002">
@@ -43,7 +84,18 @@ const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" style={{width:"200px"}} placeholder='please enter your name' value={formData.name} onChange={handleChange} />
+            <input 
+              type="text" 
+              id="name" 
+              name="name" 
+              style={{width:"220px"}} 
+              placeholder='please enter your name' 
+              value={formData.name} 
+              onChange={handleChange}
+              onFocus={handleFocus}
+            /><br />
+            {generalError && <span className="error-message" style={{fontSize:"12px", color:"orange"}}>*{generalError}</span>}
+            {errors.name && <span className="error-message" style={{fontSize:"13px", color:"orange"}}>*{errors.name}</span>}
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
@@ -52,14 +104,17 @@ const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
                 type={showPassword ? 'text' : 'password'}
                 id="password"
                 name="password"
-                placeholder='please enter your password '
+                placeholder='please enter your password'
                 value={formData.password}
                 onChange={handleChange}
+                onFocus={handleFocus}
               />
               <span className="password-icon" onClick={togglePasswordVisibility}>
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </span>
             </div>
+            {generalError && <span className="error-message" style={{fontSize:"12px", color:"orange"}}>*{generalError}</span>}
+            {errors.password && <span className="error-message" style={{fontSize:"13px",color:"orange"}}>*{errors.password}</span>}
           </div>
           {formData.name.toLowerCase() === "murali" && formData.password === "murali@@1002" ? (
             <Link to="/components/AllData/AllData">
@@ -70,7 +125,7 @@ const PopupForm = ({ onClose, loggedIn, setLoggedIn }) => {
               <button type="submit" onClick={formClose}>Submit</button>
             </Link>
           ) : (
-            " "
+            <button type="submit">Submit</button>
           )}
         </form>
       </div>
